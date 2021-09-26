1303f7f72a6083f24efa7c0bc5e3bb4939134b6f
Nahid51 <hasannahidnahid@gmail.com>
2021-09-26 22:19:12 +0600

    final push

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Carousel } from 'react-bootstrap';
 import Book from '../Book/Book';
 import BookCart from '../BookCart/BookCart';
 
@@ -40,6 +41,84 @@ const Books = () => {
                         <BookCart
                             addBookToCart={addBookToCart}
                         ></BookCart>
+
+                        <div>
+                            <div className='mt-3 w-75'>
+                                <Carousel>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2010/01/8998388.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2011/10/0312863551.01.LZ_.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2015/02/0307346617.01.LZ_.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/books/BC_0441007317.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2010/11/0156001314.01.LZ_.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2012/04/war-and-peace-hardback-768x1389.jpeg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2010/01/0140268529.01.LZ_.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2010/01/71ILek0FyCL-768x1200.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://fivebooks.com/app/uploads/2010/01/006147875X.01.LZ_.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                    <Carousel.Item>
+                                        <img
+                                            className="d-block w-100"
+                                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/The_Time_Machine_%28H._G._Wells%2C_William_Heinemann%2C_1895%29_title_page.jpg/330px-The_Time_Machine_%28H._G._Wells%2C_William_Heinemann%2C_1895%29_title_page.jpg"
+                                            alt="First slide"
+                                        />
+                                    </Carousel.Item>
+
+                                </Carousel>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,44 @@
+import Button from '@restart/ui/esm/Button';
 import React from 'react';
+import { Container, Form, FormControl, Nav, Navbar } from 'react-bootstrap';
 import './Header.css'
 
 const Header = () => {
     return (
-        <div className='rounded text-center m-5 py-3 header'>
-            <h3 className='fw-bold'>Collect Books from My<span className='budge'> BookLand Library</span></h3>
-            <p>Create your own library and make yourself interested in reading books.</p>
-            <h5 className='fw-bold budge'>Budge for Library: &#2547; 4000</h5>
+
+        <div>
+            <Navbar bg="secondary" variant="dark" expand="lg">
+                <Container fluid>
+                    <Navbar.Brand href="#">BookLand</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action1">Shop</Nav.Link>
+                            <Nav.Link href="#action1">Contact</Nav.Link>
+
+                        </Nav>
+                        <Form className="d-flex">
+                            <FormControl
+                                type="search"
+                                placeholder="Search"
+                                className="me-2"
+                                aria-label="Search"
+                            />
+                            <Button variant="outline-success">Search</Button>
+                        </Form>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <div className='rounded text-center m-5 py-3 header'>
+                <h3 className='fw-bold'>Collect Books from My<span className='budge'> BookLand Library</span></h3>
+                <p>Create your own library and make yourself interested in reading books.</p>
+                <h5 className='fw-bold budge'>Budge for Library: &#2547; 4000</h5>
+            </div>
         </div>
     );
 };
